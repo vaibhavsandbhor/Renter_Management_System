f8387fe6487fc44a8b3a18f31f0c1955f4b04b2e
vaibhav030328 <vaibhavsandbhor0303@gmail.com>
2025-10-20 21:17:33 +0530

    databasebackup

--- RentManagementSystem/src/main/java/com/rentmanagementsystem/controller/RenterController.java
+++ RentManagementSystem/src/main/java/com/rentmanagementsystem/controller/RenterController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rentmanagementsystem.entitiy.RentTransactionEntity;
 import com.rentmanagementsystem.entitiy.Renters;
 import com.rentmanagementsystem.service.RenterService;
 @RestController
@@ -23,4 +24,11 @@ public class RenterController {
 	{
       return renterservice.getAllRenters();
 	}
+	
+	
+//	public List<RentTransactionEntity> getAllTransctionDetails()
+//	{
+//		return ;
+//		
+//	}
 }
